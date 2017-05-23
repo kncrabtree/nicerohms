c92c3a8d7921b91047a95102796d82f47df2190e
Charlie <cmarkus2@illinoise.edu>
2017-05-23 10:39:51 -0500

    commit before adding new scan method

--- scanconfigwidget.cpp
+++ scanconfigwidget.cpp
@@ -11,7 +11,7 @@
 #include <QApplication>
 #include <QMessageBox>
 
-#include <QDebug>
+
 ScanConfigWidget::ScanConfigWidget(Scan::ScanType t, QWidget *parent) :
      QWidget(parent),
 	ui(new Ui::ScanConfigWidget), d_type(t)
